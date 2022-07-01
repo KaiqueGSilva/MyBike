4b490fa52c41afe99d84ff5e7776f5ebd5f2072a
KaiqueGSilva <kaka.gon.silva@gmail.com>
2022-07-01 15:52:29 -0300

    catalogo

--- app/src/main/java/br/com/senac/mybike/CatalogoActivity.java
+++ app/src/main/java/br/com/senac/mybike/CatalogoActivity.java
@@ -1,14 +1,32 @@
 package br.com.senac.mybike;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class CatalogoActivity extends AppCompatActivity {
+    RecyclerView idRecCatalogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalogo_layout);
+
+        idRecCatalogo = findViewById(R.id.idRecCatalogo);
+
+        //Montagem do layout que será utilizado na lista
+
+        idRecCatalogo.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+        //Vai deixar a Lista mais eficiente
+        idRecCatalogo.hasFixedSize();
+
+        //Instanciar o adaptador
+        MyAdapter adapter =  new MyAdapter();
+
+        //Executar a montagem da lista com o modelo
+        idRecCatalogo.setAdapter(adapter);
     }
 }
